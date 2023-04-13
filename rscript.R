2417ec07ecee1b41a28f5c580ef75e0d0fac2cd4
zhaxinge <zhaxinge@upenn.edu>
2023-04-12 20:37:05 -0400

    Update rscript.R

--- rscript.R
+++ rscript.R
@@ -29,21 +29,28 @@ for(i in seq_along(packages)){
   Load.fun(packge)
 }
 
-plotTheme <- theme(
-  plot.title =element_text(size=12),
-  plot.subtitle = element_text(size=8),
-  plot.caption = element_text(size = 6),
-  axis.text.x = element_text(size = 10, angle = 45, hjust = 1),
-  axis.text.y = element_text(size = 10),
-  axis.title.y = element_text(size = 10),
-  # Set the entire chart region to blank
-  panel.background=element_blank(),
-  plot.background=element_blank(),
-  #panel.border=element_rect(colour="#F0F0F0"),
-  # Format the grid
-  panel.grid.major=element_line(colour="#D0D0D0",size=.75),
-  axis.ticks=element_blank())
-
+plotTheme <- function(base_size = 12, title_size = 12) {
+  theme(
+   text = element_text( color = "black"),
+   plot.title = element_text(size = title_size, colour = "black"), 
+        plot.subtitle = element_text(face="italic"),
+       plot.caption = element_text(hjust=0),
+         axis.ticks = element_blank(),
+         panel.background = element_blank(),
+         panel.grid.minor = element_line("grey90", size = 0.01),
+         panel.grid.major = element_line("grey90", size = 0.01),
+         panel.border = element_rect(colour = "white" , fill=NA, size=0),
+         strip.background = element_rect(color = "white",fill='white'),
+         strip.text = element_text(size=12),
+         axis.title = element_text(size=12),
+         axis.text = element_text(size=10),
+         plot.background = element_blank(),
+         legend.background = element_blank(),
+         legend.title = element_text(colour = "black", face = "italic"),
+         legend.text = element_text(colour = "black", face = "italic"),
+         strip.text.x = element_text(size = 8)
+       )
+   }
 mapTheme <- theme(plot.title =element_text(size=12),
                   plot.subtitle = element_text(size=8),
                   plot.caption = element_text(size = 6),
